4a4b79c666cb2a17e723b00322ff5251b08abbad
inlancersystem4 <inlancersystem4@gmail.com>
2024-01-16 16:09:17 +0530

    add code

---
+++ assets/js/root.js
@@ -0,0 +1,33 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                theme_color1: '#54BD95',
+                theme_color2: '#191A15',
+                theme_color3: '#000000',
+                theme_color4: '#A6A6A6',
+                theme_color5: '#FFFFFF',
+                theme_color6: '#161C28',
+                theme_color7: '#F9F8FE',
+                theme_color8: '#54BE96',
+                theme_color9: '#6BC2A1',
+                theme_color10: '#6BC2A1',
+                theme_color11: '#F9FAFB',
+                theme_color12: '#85DAB9',
+                theme_color13: '#222938',
+            },
+            fontSize: {
+                'section_heading': ['50px', {
+                    lineHeight: 'normal',
+                    fontWeight: '600',
+                }],
+                'section_title': ['30px', {
+                    lineHeight: 'normal',
+                }],
+                'section_sub-title': ['18px', {
+                    fontWeight: '500',
+                }],
+            },
+        }
+    }
+}
